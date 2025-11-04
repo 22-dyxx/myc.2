61fad6421d93234a2d3c94f7c71576566e4c9ef4
22-dyxx <3060013495@qq.com>
2025-11-04 19:43:42 +0800

    Add files via upload

---
+++ 十个数字从小到大排序.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a[10];
+	int i,j,t;
+	printf("请输入十个数: \n");
+	for(i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	printf("\n");
+	for(j=0;j<9;j++)
+	 for(i=0;i<9-j;i++)
+	  if(a[i]>a[i+1])
+	  {
+	  	t=a[i];a[i]=a[i+1];a[i+1]=t;
+	  }
+	printf("结果为:\n");
+	for(i=0;i<10;i++)
+	printf("%d ",a[i]); 
+	return 0;
+}
